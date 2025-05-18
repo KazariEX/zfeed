921e96d63012d16d5743359255b1f44e8e9915e8
山吹色御守 <85992002+KazariEX@users.noreply.github.com>
2025-05-19 07:10:46 +0800

    chore: init

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+import antfu from "@antfu/eslint-config";
+import zin from "@zinkawaii/eslint-config";
+
+export default antfu({
+    pnpm: true,
+    rules: {
+        ...zin.standard,
+        ...zin.recommended,
+        ...zin.stylistic,
+        ...zin.patch,
+    },
+});
