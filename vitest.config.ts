13b99e11f6755465d4ba520a7a0f1932ca802868
山吹色御守 <85992002+KazariEX@users.noreply.github.com>
2025-05-21 15:07:50 +0800

    test: use `vite-tsconfig-paths`

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,10 @@
 import vue from "@vitejs/plugin-vue";
+import paths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
     plugins: [
+        paths(),
         vue({
             template: {
                 transformAssetUrls: false,
